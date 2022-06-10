82480c3427da26818224a8127468329747f87573
Ytellon Macedo <ytellonmacedo@gmail.com>
2022-06-09 23:53:24 -0300

    feat: adicionado botões que removem todos os filtros

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -12,6 +12,7 @@ function Form() {
     'rotation_period',
     'surface_water',
   ]);
+
   const { setFilterByNumericValues, filterByNumericValues } = useContext(Context);
 
   const handleNumericFilter = () => {
@@ -25,50 +26,84 @@ function Form() {
     setFilterByNumericValues([...filterByNumericValues, filteredPlanets]);
   };
 
+  const removeFilter = (columnFilter) => {
+    const filterByNumericValuesCopy = filterByNumericValues.filter(
+      (filter) => filter.column !== columnFilter,
+    );
+    setFilterByNumericValues(filterByNumericValuesCopy);
+    setOptionSelected([...optionSelected, columnFilter]);
+  };
+
+  const removeAllFilters = () => {
+    setFilterByNumericValues([]);
+    setOptionSelected([
+      'population',
+      'orbital_period',
+      'diameter',
+      'rotation_period',
+      'surface_water',
+    ]);
+  };
+
   return (
-    <form>
-      <select
-        onChange={ (event) => setColumn(event.target.value) }
-        data-testid="column-filter"
-      >
-        {optionSelected.map((option) => (
-          <option key={ option } value={ option }>
-            {option}
-          </option>
+    <div>
+      <form>
+        <select
+          onChange={ (event) => setColumn(event.target.value) }
+          data-testid="column-filter"
+        >
+          {optionSelected.map((option) => (
+            <option key={ option } value={ option }>
+              {option}
+            </option>
+          ))}
+        </select>
+        <select
+          onChange={ (event) => setComparison(event.target.value) }
+          data-testid="comparison-filter"
+        >
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+        <input
+          onChange={ (event) => setValue(event.target.value) }
+          data-testid="value-filter"
+          type="number"
+          placeholder="0"
+          value={ value }
+        />
+        <button
+          onClick={ handleNumericFilter }
+          data-testid="button-filter"
+          type="button"
+        >
+          Filter
+        </button>
+        <button
+          data-testid="button-remove-filters"
+          type="button"
+          onClick={ removeAllFilters }
+        >
+          Remove Filters
+        </button>
+      </form>
+      <section>
+        {filterByNumericValues.map((filterPlanet, index) => (
+          <div data-testid="filter" key={ index }>
+            <p>
+              {`${filterPlanet.column} ${filterPlanet.comparison} ${filterPlanet.value}`}
+            </p>
+            <button
+              onClick={ () => removeFilter(filterPlanet.column) }
+              type="button"
+            >
+              X
+            </button>
+          </div>
         ))}
-      </select>
-      <select
-        onChange={ (event) => setComparison(event.target.value) }
-        data-testid="comparison-filter"
-      >
-        <option value="maior que">maior que</option>
-        <option value="menor que">menor que</option>
-        <option value="igual a">igual a</option>
-      </select>
-      <input
-        onChange={ (event) => setValue(event.target.value) }
-        data-testid="value-filter"
-        type="number"
-        placeholder="0"
-        value={ value }
-      />
-      <button
-        onClick={ handleNumericFilter }
-        data-testid="button-filter"
-        type="button"
-      >
-        Filter
-      </button>
-      <div>
-        { filterByNumericValues.map((filterPlanet, index) => (
-          <p key={ index }>
-            {
-              `${filterPlanet.column} ${filterPlanet.comparison} ${filterPlanet.value}`
-            }
-          </p>
-        )) }
-      </div>
-    </form>
+      </section>
+    </div>
   );
 }
 
